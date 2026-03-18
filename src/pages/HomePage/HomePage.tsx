d7948d2a07c7b115a0e899cf7a7b9a8a10c13b44
sofia-grigoreva <sofya.grigorevaa@gmail.com>
2026-03-18 15:00:12 +0300

    feat: add top of home page component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,61 +1,13 @@
 import React from 'react';
+import HomePageTop from '../../components/HomePageTop/HomePageTop';
 import InTrends from '../../components/InTrends/InTrends';
+import { trendingVideos } from '../../mocks/films';
 import styles from './HomePage.module.scss';
-
-const trendingVideos = [
-	{
-		src: '#',
-		title: 'Фильм 1',
-	},
-	{
-		src: '#',
-		title: 'Фильм 2',
-	},
-	{
-		src: '#',
-		title: 'Фильм 3',
-	},
-	{
-		src: '#',
-		title: 'Фильм 4',
-	},
-	{
-		src: '#',
-		title: 'Фильм 5',
-	},
-	{
-		src: '#',
-		title: 'Фильм 6',
-	},
-	{
-		src: '#',
-		title: 'Фильм 7',
-	},
-	{
-		src: '#',
-		title: 'Фильм 8',
-	},
-	{
-		src: '#',
-		title: 'Фильм 9',
-	},
-	{
-		src: '#',
-		title: 'Фильм 10',
-	},
-	{
-		src: '#',
-		title: 'Фильм 11',
-	},
-	{
-		src: '#',
-		title: 'Фильм 12',
-	},
-];
 
 const HomePage: React.FC = () => {
 	return (
 		<div className={styles.page}>
+			<HomePageTop />
 			<InTrends videos={trendingVideos} />
 		</div>
 	);
